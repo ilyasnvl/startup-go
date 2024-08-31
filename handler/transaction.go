8128ba58ddaff8310ee231dca5d52ca8eab4123e
ilyasnvl <ilyasnauval96@gmail.com>
2024-08-31 10:41:07 +0700

    payment notification

--- handler/transaction.go
+++ handler/transaction.go
@@ -102,3 +102,23 @@ func (h *transactionHandler) CreateTransaction(c *gin.Context) {
 	response := helper.APIResponse("Success to create Transaction", http.StatusOK, "success", transaction.FormatTransaction(newTransaction))
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *transactionHandler) GetNotification(c *gin.Context) {
+	var input transaction.TransactionNotificationInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to proccess notification", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	err = h.service.ProccessPayment(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to proccess notification", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	c.JSON(http.StatusOK, input)
+}
